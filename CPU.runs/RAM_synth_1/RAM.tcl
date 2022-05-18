5a85f7f297a21232bca5b2014b3f3e8f917b9d96
Sunrui <1660901102@qq.com>
2022-05-18 22:15:49 +0800

    添加了两个让ip核初始化的coe文件

--- CPU.runs/RAM_synth_1/RAM.tcl
+++ CPU.runs/RAM_synth_1/RAM.tcl
@@ -24,15 +24,15 @@ set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
 set_msg_config -source 4 -id {IP_Flow 19-2162} -severity warning -new_severity info
-set_property webtalk.parent_dir C:/Users/Lenovo/CPU/CPU.cache/wt [current_project]
-set_property parent.project_path C:/Users/Lenovo/CPU/CPU.xpr [current_project]
-set_property XPM_LIBRARIES XPM_MEMORY [current_project]
+set_property webtalk.parent_dir D:/VivadoProCS202/CPU/CPU.cache/wt [current_project]
+set_property parent.project_path D:/VivadoProCS202/CPU/CPU.xpr [current_project]
+set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
-set_property ip_output_repo c:/Users/Lenovo/CPU/CPU.cache/ip [current_project]
+set_property ip_output_repo d:/VivadoProCS202/CPU/CPU.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_ip -quiet c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM.xci
-set_property used_in_implementation false [get_files -all c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_ooc.xdc]
+read_ip -quiet D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM.xci
+set_property used_in_implementation false [get_files -all d:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_ooc.xdc]
 
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -45,7 +45,7 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
 read_xdc dont_touch.xdc
 set_property used_in_implementation false [get_files dont_touch.xdc]
 
-set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir C:/Users/Lenovo/CPU/CPU.runs/RAM_synth_1 -new_name RAM -ip [get_ips RAM]]
+set cached_ip [config_ip_cache -export -no_bom -use_project_ipc -dir D:/VivadoProCS202/CPU/CPU.runs/RAM_synth_1 -new_name RAM -ip [get_ips RAM]]
 
 if { $cached_ip eq {} } {
 
@@ -84,32 +84,32 @@ write_checkpoint -force -noxdef RAM.dcp
 create_report "RAM_synth_1_synth_report_utilization_0" "report_utilization -file RAM_utilization_synth.rpt -pb RAM_utilization_synth.pb"
 
 if { [catch {
-  file copy -force C:/Users/Lenovo/CPU/CPU.runs/RAM_synth_1/RAM.dcp c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM.dcp
+  file copy -force D:/VivadoProCS202/CPU/CPU.runs/RAM_synth_1/RAM.dcp D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.v
+  write_verilog -force -mode synth_stub D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.vhdl
+  write_vhdl -force -mode synth_stub D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.v
+  write_verilog -force -mode funcsim D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -119,46 +119,46 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force C:/Users/Lenovo/CPU/CPU.runs/RAM_synth_1/RAM.dcp c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM.dcp
+  file copy -force D:/VivadoProCS202/CPU/CPU.runs/RAM_synth_1/RAM.dcp D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force C:/Users/Lenovo/CPU/CPU.runs/RAM_synth_1/RAM_stub.v c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.v
+  file rename -force D:/VivadoProCS202/CPU/CPU.runs/RAM_synth_1/RAM_stub.v D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/Lenovo/CPU/CPU.runs/RAM_synth_1/RAM_stub.vhdl c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.vhdl
+  file rename -force D:/VivadoProCS202/CPU/CPU.runs/RAM_synth_1/RAM_stub.vhdl D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/Lenovo/CPU/CPU.runs/RAM_synth_1/RAM_sim_netlist.v c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.v
+  file rename -force D:/VivadoProCS202/CPU/CPU.runs/RAM_synth_1/RAM_sim_netlist.v D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/Lenovo/CPU/CPU.runs/RAM_synth_1/RAM_sim_netlist.vhdl c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.vhdl
+  file rename -force D:/VivadoProCS202/CPU/CPU.runs/RAM_synth_1/RAM_sim_netlist.vhdl D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 }; # end if cached_ip 
 
-if {[file isdir C:/Users/Lenovo/CPU/CPU.ip_user_files/ip/RAM]} {
+if {[file isdir D:/VivadoProCS202/CPU/CPU.ip_user_files/ip/RAM]} {
   catch { 
-    file copy -force c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.v C:/Users/Lenovo/CPU/CPU.ip_user_files/ip/RAM
+    file copy -force D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.v D:/VivadoProCS202/CPU/CPU.ip_user_files/ip/RAM
   }
 }
 
-if {[file isdir C:/Users/Lenovo/CPU/CPU.ip_user_files/ip/RAM]} {
+if {[file isdir D:/VivadoProCS202/CPU/CPU.ip_user_files/ip/RAM]} {
   catch { 
-    file copy -force c:/Users/Lenovo/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.vhdl C:/Users/Lenovo/CPU/CPU.ip_user_files/ip/RAM
+    file copy -force D:/VivadoProCS202/CPU/CPU.srcs/sources_1/ip/RAM/RAM_stub.vhdl D:/VivadoProCS202/CPU/CPU.ip_user_files/ip/RAM
   }
 }
